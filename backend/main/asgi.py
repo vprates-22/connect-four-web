3f5924d07b5d68037a92611b469552c8746d91a6
Victor Prates <vprates2222@gmail.com>
2024-09-26 14:36:32 -0300

    add: backend

--- backend/main/asgi.py
+++ backend/main/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for backend project.
+ASGI config for main project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -10,10 +10,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
-application = ProtocolTypeRouter({
-    'http' : get_asgi_application()
-})
+application = get_asgi_application()
