244975992145ec3fee4114610326ca7f4325d4af
Victor Prates <vprates2222@gmail.com>
2024-10-12 17:15:46 -0300

    update: PopUp

--- frontend/src/components/PopUp/CreatePopUp.tsx
+++ frontend/src/components/PopUp/CreatePopUp.tsx
@@ -1,16 +1,48 @@
 import './CreatePopUp.css'
 
+type CreateOptParams = {
+    idName:string,
+    minVal:number,
+    maxVal:number
+}
+
+const CreateOptions = (params:CreateOptParams) => {
+    const idName:string = params.idName
+    const minVal:number = params.minVal
+    const maxVal:number = params.maxVal
+
+
+    const arrayLength = maxVal - minVal + 1
+    const options = [...Array(arrayLength).keys()].map(i => i + minVal);
+    
+    return(
+        <>
+            <select className='BoardSelect' id={idName}> 
+                {options.map(size => <option value={size}>{size}</option>)}
+            </select>
+        </>
+
+    );
+}
+
 const CreatePopUp = () => {
     return (
         <div className="PopUpBox">
-            <div className='BoardParams'>
-                Height <input type="number" name="" id="" min="5" max="10"/>
-            </div>
-            <div className='BoardParams'>
-                Width <input type="number" name="" id="" min="6" max="12"/>
-            </div>
-            <div className='SendData'>
-                <input type="button" value="Create" />
+            <h1 className='Title'>Create Room</h1>
+            <form className='GameParams'>
+                <div className='BoardParams'>
+                    <div className='DimParams'>
+                        <p>Height.............................</p> 
+                        <CreateOptions {...{idName:'Height', minVal:6, maxVal:10}}/>
+                    </div>
+                    <div className='DimParams'>
+                        <p>Width..............................</p> 
+                        <CreateOptions {...{idName:'Height', minVal:7, maxVal:12}}/>
+                    </div>
+                </div>
+            </form>
+            <div className='SubmitDiv'>
+                <button className='SubmitButton' type='submit'>Continue</button>
             </div>
         </div>
     );
